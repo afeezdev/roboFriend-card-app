ba8c7a4254c78a2e12264125a4cdb46ecb94ffd6
afeezdev <afeezdev@gmail.com>
2020-03-05 13:41:15 +0100

    fixing the loading bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -39,7 +39,7 @@ class App extends Component{
                 <Scroll>
                     <CardList robots = {filteredRobots}/>
                 </Scroll>
-                
+    
             </div>
         );
         
